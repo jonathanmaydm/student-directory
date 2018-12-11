06b77093e8da0eb0e012c76bb4cc2a962d53a26a
Jonathan May <jonathan.may@devmountain.com>
2018-12-11 09:13:17 -0600

    Add StudentsList

---
+++ app/src/components/StudentsList.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class StudentsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://dm20.now.sh/students")
+      .then(response => this.setState({ students: response.data }));
+  }
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap"
+        }}
+      >
+        {this.state.students.map((student, i) => (
+          <a href={`/students/${student.id || i}`}>
+            <div className="card">
+              <h2>{student.name}</h2>
+              <h3>{student.from}</h3>
+            </div>
+          </a>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default StudentsList;
